b13bcbeb61841e8146a8e0251ff77d4d51432f03
Cosimo Lupo <cosimo.lupo89@gmail.com>
2021-09-08 16:08:21 +0200

    Initialization

--- lttb.py
+++ lttb.py
@@ -1,76 +1,122 @@
 """
-    © 2021 This work is licensed under a CC-BY-NC-SA license.
-    Title: ---
-    Authors: Cristiano Capone et al.
+	© 2021 This work is licensed under a CC-BY-NC-SA license.
+	Title: ---
+	Authors: Cristiano Capone et al.
 """
 
 import numpy as np
 from tqdm import trange
 
 class LTTB:
-    """
-        This is the base Model ...
-    """
+	"""
+		This is the base Model ...
+	"""
+	
+	def __init__ (self, par):
+		# This are the network size N, input I, output O and max temporal span T
+		self.N, self.I, self.O, self.T = par['shape']
 
-    def __init__ (self, par):
-        # This are the network size N, input I, output O and max temporal span T
-        self.N, self.I, self.O, self.T = par['shape']
+		self.dt = par['dt']
 
-        self.dt = par['dt']
+		self.itau_m = np.exp (-self.dt / par['tau_m'])
+		self.itau_s = np.exp (-self.dt / par['tau_s'])
+		self.itau_ro = np.exp (-self.dt / par['tau_ro'])
+		self.itau_star = np.exp (-self.dt / par['tau_star'])
 
-        self.itau_m = np.exp (-self.dt / par['tau_m'])
-        self.itau_s = np.exp (-self.dt / par['tau_s'])
-        self.itau_ro = np.exp (-self.dt / par['tau_ro'])
-        self.itau_star = np.exp (-self.dt / par['tau_star'])
+		self.dv = par['dv']
 
-        self.dv = par['dv']
+		# This is the network connectivity matrix
+		self.J = np.random.normal (0., par['sigma_Jrec'], size = (self.N, self.N))
+		# self.J = np.zeros ((self.N, self.N))
 
-        # This is the network connectivity matrix
-        self.J = np.random.normal (0., par['sigma_Jrec'], size = (self.N, self.N))
-        # self.J = np.zeros ((self.N, self.N))
+		# This is the network input, teach and output matrices
+		self.Jin = np.random.normal (0., par['sigma_input'], size = (self.N, self.I))
+		self.Jteach = np.random.normal (0., par['sigma_teach'], size = (self.N, self.O))
 
-        # This is the network input, teach and output matrices
-        self.Jin = np.random.normal (0., par['sigma_input'], size = (self.N, self.I))
-        self.Jteach = np.random.normal (0., par['sigma_teach'], size = (self.N, self.O))
+		# This is the hint signal
+		try:
+			self.H = par['hint_shape']
+			self.Jhint = np.random.normal (0., par['sigma_hint'], size = (self.N, self.H))
+		except KeyError:
+			self.H = 0
+			self.Jhint = None
+		
+		self.Jout = np.random.normal (0., par['sigma_Jout'], size = (self.O, self.N))
+		# self.Jout = np.zeros ((self.O, self.N))
+		
+		# Remove self-connections
+		np.fill_diagonal (self.J, 0.)
+		
+		# Impose reset after spike
+		self.s_inh = -par['s_inh']
+		self.Jreset = np.diag (np.ones (self.N) * self.s_inh)
+		
+		# This is the external field
+		h = par['h']
+		
+		assert type (h) in (np.ndarray, float, int)
+		self.h = h if isinstance (h, np.ndarray) else np.ones (self.N) * h
+		
+		# Membrane potential
+		self.H = np.ones (self.N) * par['Vo']
+		self.Vo = par['Vo']
+		
+		# These are the spikes train and the filtered spikes train
+		self.S = np.zeros (self.N)
+		self.S_hat = np.zeros (self.N)
+		
+		# This is the single-time output buffer
+		self.out = np.zeros (self.N)
+		
+		# Here we save the params dictionary
+		self.par = par
+		
+		self.b = 100
 
-        # This is the hint signal
-        try:
-            self.H = par['hint_shape']
-            self.Jhint = np.random.normal (0., par['sigma_hint'], size = (self.N, self.H))
-        except KeyError:
-            self.H = 0
-            self.Jhint = None
+		self.t = 0
+		
+		self.apicalFactor = 1
 
-        self.Jout = np.random.normal (0., par['sigma_Jout'], size = (self.O, self.N))
-        # self.Jout = np.zeros ((self.O, self.N))
+		self.me = 0.01
+		self.mi = 4 * self.me
 
-        # Remove self-connections
-        np.fill_diagonal (self.J, 0.)
+		self.href = -5
 
-        # Impose reset after spike
-        self.s_inh = -par['s_inh']
-        self.Jreset = np.diag (np.ones (self.N) * self.s_inh)
+		self.S_soma = np.zeros((self.N,self.T))
+		self.S_apic_dist = np.zeros((self.N,self.T))
+		self.S_apic_prox = np.zeros((self.N,self.T))
+		self.W = np.zeros((self.N,self.T))
 
-        # This is the external field
-        h = par['h']
+		self.S_wind_apic = np.zeros((self.N,self.T))
+		self.S_wind_cont = np.zeros((self.N,self.T))
+		self.S_wind_soma = np.zeros((self.N,self.T))
+		self.S_wind = np.zeros((self.N,self.T))
 
-        assert type (h) in (np.ndarray, float, int)
-        self.h = h if isinstance (h, np.ndarray) else np.ones (self.N) * h
+		self.B_filt = np.zeros((self.N,self.T))
+		self.B_filt_rec = np.zeros((self.N,self.T))
 
-        # Membrane potential
-        self.H = np.ones (self.N) * par['Vo']
-        self.Vo = par['Vo']
+		self.S_wind_targ = np.zeros((self.N,self.T))
 
-        # These are the spikes train and the filtered spikes train
-        self.S = np.zeros (self.N)
-        self.S_hat = np.zeros (self.N)
+		self.S_filt_targ = np.zeros((self.N,self.T))
+		self.S_filt = np.zeros((self.N,self.T))
+		self.S_filt_soma = np.zeros((self.N,self.T))
+		self.S_filt_apic = np.zeros((self.N,self.T))
+		self.S_filt_apicCont = np.zeros((self.N,self.T))
+		self.S_filtRO = np.zeros((self.N,self.T))
 
-        # This is the single-time output buffer
-        self.out = np.zeros (self.N)
+		self.S_wind_targ_filt = np.zeros((self.N,self.T))
 
-        # Here we save the params dictionary
-        self.par = par
+		self.Vapic = np.zeros((self.N,self.T)) + h
+		self.VapicRec = np.zeros((self.N,self.T)) + h
+		self.Vsoma = np.zeros((self.N,self.T)) + h
 
+		self.Vreset = -20
+		self.VresetApic = -80*2
+		self.apicalThreshold = .05/4/4
+		self.somaticThreshold = .05/2
+		self.burstThreshold = .05/4
+		self.targThreshold = .08
+		
     def _sigm (self, x, dv = None):
         if dv is None:
             dv = self.dv
